160c151b174dd2dd2a678cb28b667eac14f988ee
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2011-09-09 09:59:59 +0000

    ADD: Added x64 target to UnitTestCpp. ADD: UnitTestCpp added. FIX: Freeimage debug lib problem fixed.
    svn path=/; revision=9

--- Source/libUnitTestCpp/build.bat
+++ Source/libUnitTestCpp/build.bat
@@ -1,6 +1,6 @@
 :Initialization
 @echo off
-set ProjectName=libFreeImage
+set ProjectName=libUnitTestCpp
 @echo [ZEBuild Externals] Info : Starting to build external %ProjectName%.
  
 :CleanUp
@@ -13,29 +13,31 @@ rmdir /s /q Output
 :Build
 @echo [ZEBuild Externals] Info : Building %ProjectName%.
 
-vcbuild /Clean Source\FreeImage.2008.sln
+vcbuild /Upgrade Source\UnitTest++.vsnet2005.sln
+if %ERRORLEVEL% NEQ 0 GOTO Error
+
+vcbuild /Clean Source\UnitTest++.vsnet2005.sln
 if %ERRORLEVEL% NEQ 0 GOTO Error
 del /q /f Source\Dist
 
-vcbuild Source\FreeImage.2008.sln "Release|Win32"
+vcbuild Source\UnitTest++.vsnet2005.sln "Release|Win32"
 if %ERRORLEVEL% NEQ 0 GOTO Error
-xcopy /r /y Source\Dist\freeimage.lib Output\Lib\Win32\
-move  Output\Lib\Win32\freeimage.lib Output\Lib\Win32\libfreeimage.lib
-xcopy /r /y Source\Dist\FreeImage.dll Output\Dll\Win32\
+xcopy /r /y Source\Release\UnitTest++.vsnet2005.lib Output\Lib\Win32\
+move  Output\Lib\Win32\UnitTest++.vsnet2005.lib Output\Lib\Win32\libUnitTestCpp.lib
 
-vcbuild /Clean Source\FreeImage.2008.sln
+vcbuild /Clean Source\UnitTest++.vsnet2005.sln
 if %ERRORLEVEL% NEQ 0 GOTO Error
 del /q /f Source\Dist
 
-vcbuild Source\FreeImage.2008.sln "Release|x64"
+vcbuild Source\UnitTest++.vsnet2005.sln "Release|x64"
 if %ERRORLEVEL% NEQ 0 GOTO Error
-xcopy /r /y Source\Dist\freeimage.lib Output\Lib\Win64\
-move  Output\Lib\Win64\freeimage.lib Output\Lib\Win64\libfreeimage.lib
-xcopy /r /y Source\Dist\FreeImage.dll Output\Dll\Win64\
+xcopy /r /y Source\x64\Release\UnitTest++.vsnet2005.lib Output\Lib\Win64\
+move  Output\Lib\Win64\UnitTest++.vsnet2005.lib Output\Lib\Win64\libUnitTestCpp.lib
+
 
 :CopyOutput
 @echo [ZEBuild Externals] Info : Copying output of %ProjectName%.
-xcopy /r /y Source\Dist\FreeImage.h Output\Include\
+xcopy /r /y Source\src\*.h Output\Include\UnitTestCpp\
 goto End
 
 :Error
