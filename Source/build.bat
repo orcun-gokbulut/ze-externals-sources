cf07dc95558591e8c7505657106b53888bc719a5
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2011-10-11 15:45:45 +0000

    ADD: libQT added.
    svn path=/; revision=24

--- Source/build.bat
+++ Source/build.bat
@@ -83,6 +83,12 @@ if %ERRORLEVEL% NEQ 0 GOTO Error
 cd ..
 xcopy /r /y /e libPhysX3\Output ..\Output\
 
+cd libQT
+call build.bat
+if %ERRORLEVEL% NEQ 0 GOTO Error
+cd ..
+xcopy /r /y /e libQT\Output ..\Output\
+
 cd libUnitTestCpp
 call build.bat
 if %ERRORLEVEL% NEQ 0 GOTO Error
---
+++ Source/libQt/build.bat
@@ -0,0 +1,23 @@
+:Initialization
+@echo off
+set ProjectName=libQt
+@echo [ZEBuild Externals] Info : Starting to build external %ProjectName%.
+ 
+:CleanUp
+@echo [ZEBuild Externals] Info : Cleaning up %ProjectName%.
+
+:Build32
+@echo [ZEBuild Externals] Info : Building %ProjectName%.
+:Build64
+
+:CopyOutput
+goto End
+
+:Error
+cd ..
+@echo [ZEBuild Externals] Error : Error occured while building %ProjectName%. 
+exit /b 1
+
+:End
+@echo [ZEBuild Externals] Success : %ProjectName% build external is completed successfully.
+exit /b 0
