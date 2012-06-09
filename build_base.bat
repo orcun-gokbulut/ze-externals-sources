fbdca397d1deda581a7cedc0bfa1adb86d0f65d8
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2012-06-09 07:07:05 +0000

    IMP: Build scripts has been improved.
    svn path=/; revision=45

--- build_base.bat
+++ build_base.bat
@@ -1,11 +1,7 @@
 :Initialization
 @echo off
 @echo [ZEBuild Externals] Info : Building externals.
-call "C:\Program Files (x86)\Microsoft Visual Studio 10\vc\vsvars32.bat"
-
-set ZE_BUILD_CMAKE_GENERATOR=Visual Studio 10
-set ZE_BUILD_CMAKE=cmake.exe -D CMAKE_CXX_FLAGS_DEBUG:STRING="/D_DEBUG /MDd /Ob0 /Od /RTC1" - D CMAKE_C_FLAGS_DEBUG:STRING="/D_DEBUG /MDd /Ob0 /Od /RTC1"
-@echo [ZEBuild Externals] CMake path : %ZE_BUILD_CMAKE%
+call build_initialize.bat
 
 :CleanUp
 @echo [ZEBuild Externals] Info : Cleaning up externals.
--- build_check.bat
+++ build_check.bat
@@ -4,34 +4,35 @@ cd Output
 pushd .
 
 for /r %%I in (*.lib) do (
-	echo "%%~fI" | find "x86"
+	echo Checking file %%~fI
+	echo "%%~fI" | find "x86" > nul
 	if Not ErrorLevel 1 (
-		dumpbin /HEADERS "%%~fI" | find "8664 machine"
-		if Not ErrorLevel 1 echo Error: Architecture mismatch. x64 lib in x86 lib directory. File Name : "%%~fI"
+		dumpbin /HEADERS "%%~fI" | find "8664 machine" > nul
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Architecture mismatch. x64 lib in x86 lib directory."
 	)
 	
-	echo "%%~fI" | find "x64"
+	echo "%%~fI" | find "x64" > nul
 	if Not ErrorLevel 1 (
 		dumpbin /HEADERS "%%~fI" | find "14C machine"
-		if Not ErrorLevel 1 echo Error: Architecture mismatch. x64 lib in x86 lib directory. File Name : "%%~fI"
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Architecture mismatch. x64 lib in x86 lib directory."
 	)
 	
-	dumpbin /RAWDATA %%~fI | find "libcmt"""
-	if Not ErrorLevel 1 echo "Error: Runtime mismatch. Library uses static multi threaded runtime (libcmt). File Name : "%%I"
+	dumpbin /RAWDATA "%%~fI" | find "libcmt""" > nul
+	if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime mismatch. Library uses static multi threaded runtime (libcmt)."
 	
-	dumpbin /RAWDATA  %%~fI | find "libc "
-	if Not ErrorLevel 1 echo "Error: Runtime mismatch. Library uses static single threaded runtime (libc). File Name : "%%I"
+	dumpbin /RAWDATA  "%%~fI" | find "libc " > nul
+	if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime mismatch. Library uses static single threaded runtime (libc)."
 			
-	echo %%I | find "Debug"
+	echo "%%I" | find "Debug" > nul
 	if Not ErrorLevel 1 (
-		dumpbin /RAWDATA %%~fI | find /i"msvcrt"""
-		if Not ErrorLevel 1 echo Error: Runtime configuration mismatch. Library uses release runtime library instead of debug. File Name : "%%I"
+		dumpbin /RAWDATA "%%~fI" | find /i"msvcrt"""
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime configuration mismatch. Library uses release runtime library instead of debug."
 	)
 
-	echo %%I | find "Release"
+	echo %%I | find "Release" > nul
 	if Not ErrorLevel 1 (
-		dumpbin /RAWDATA %%~fI | find /i "msvcrtd"""
-		if Not ErrorLevel 1 echo Error: Runtime configuration mismatch. Library uses debug runtime library instead of release. File Name : "%%I"
+		dumpbin /RAWDATA "%%~fI" | find /i "msvcrtd"""
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime configuration mismatch. Library uses debug runtime library instead of release."
 	)
 )
 popd
@@ -40,31 +41,31 @@ for /r %%I in (*.dll) do (
 	echo "%%~fI" | find "x86"
 	if Not ErrorLevel 1 (
 		dumpbin /HEADERS "%%~fI" | find "8664 machine"
-		if Not ErrorLevel 1 echo Error: Architecture mismatch. x64 lib in x86 lib directory. File Name : "%%~fI"
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Architecture mismatch. x64 lib in x86 lib directory."
 	)
 	
 	echo "%%~fI" | find "x64"
 	if Not ErrorLevel 1 (
 		dumpbin /HEADERS "%%~fI" | find "14C machine"
-		if Not ErrorLevel 1 echo Error: Architecture mismatch. x64 lib in x86 lib directory. File Name : "%%~fI"
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Architecture mismatch. x64 lib in x86 lib directory."
 	)
 	
-	dumpbin /RAWDATA %%~fI | find "libcmt"""
-	if Not ErrorLevel 1 echo "Error: Runtime mismatch. Library uses static multi threaded runtime (libcmt). File Name : "%%I"
+	dumpbin /RAWDATA "%%~fI" | find "libcmt"""
+	if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime mismatch. Library uses static multi threaded runtime (libcmt)."
 	
-	dumpbin /RAWDATA  %%~fI | find "libc"""
-	if Not ErrorLevel 1 echo "Error: Runtime mismatch. Library uses static single threaded runtime (libc). File Name : "%%I"
+	dumpbin /RAWDATA  "%%~fI" | find "libc"""
+	if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime mismatch. Library uses static single threaded runtime (libc)."
 			
-	echo %%I | find "Debug"
+	echo "%%I" | find "Debug"
 	if Not ErrorLevel 1 (
-		dumpbin /RAWDATA %%~fI | find /i"msvcrt"""
-		if Not ErrorLevel 1 echo Error: Runtime configuration mismatch. Library uses release runtime library instead of debug. File Name : "%%I"
+		dumpbin /RAWDATA "%%~fI" | find /i"msvcrt"""
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime configuration mismatch. Library uses release runtime library instead of debug."
 	)
 
-	echo %%I | find "Release"
+	echo "%%I" | find "Release"
 	if Not ErrorLevel 1 (
-		dumpbin /RAWDATA %%~fI | find /i "msvcrtd"""
-		if Not ErrorLevel 1 echo Error: Runtime configuration mismatch. Library uses debug runtime library instead of release. File Name : "%%I"
+		dumpbin /RAWDATA "%%~fI" | find /i "msvcrtd"""
+		if Not ErrorLevel 1 echo "ERROR ERROR ERROR !!! Runtime configuration mismatch. Library uses debug runtime library instead of release."
 	)
 )
 popd
