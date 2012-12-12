836807b1d68723843efd41b64fc39f048450c6ba
Hakan Candemir <hakan.candemir@zinek.xyz>
2012-12-12 11:55:54 +0000

    ADD: build.py file for LibVprn
    svn path=/; revision=69

---
+++ Source/libvrpn/build.py
@@ -0,0 +1,48 @@
+class VprnLibrary(ZELibrary):
+    def Clean(self):
+        ZELibrary.Clean(self)
+
+    def Configure(self, Debug):
+        ZELibrary.Configure(self, Debug)
+        if (Debug != None):
+            Parameter = [ZECMakeParameter("CMAKE_BUILD_TYPE", "STRING", "Debug" if Debug else "Release"),
+                         ZECMakeParameter("VRPN_BUILD_CLIENTS", "BOOL", "0"),
+                         ZECMakeParameter("VRPN_BUILD_SERVER_LIBRARY", "BOOL", "0"),
+                         ZECMakeParameter("VRPN_BUILD_SERVERS", "BOOL", "0")]
+        else:
+            Parameter = None
+        ZEBuild.SetWorkingDirectory(ZEBuild.CurrentLibrary.BuildDirectory)
+        ZEBuild.CMake(self.SourceDirectory, Parameter)
+
+    def Compile(self, Debug):
+        ZELibrary.Compile(self, Debug)
+        ZEBuild.CMakeBuild(self.BuildDirectory, Debug)
+    def Gather(self, Debug):
+        ZELibrary.Gather(self, Debug)
+        if (Debug != None):
+            
+            if ZEBuild.Platform.MultiConfiguration:
+                ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release"))
+
+                VprnFileSource = self.BuildDirectory + ("/Debug" if Debug else "/Release") + ("/vrpn.lib" if ZEBuild.Platform.Platform == "Windows" else "/libvrpn.a")
+                VprnFileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + ("/libVrpn.lib" if ZEBuild.Platform.Platform == "Windows" else "/libVrpn.a")
+                
+                QuatFileSource = self.BuildDirectory + "/quat" + ("/Debug" if Debug else "/Release") + ("/quat.lib" if ZEBuild.Platform.Platform == "Windows" else "/libquat.a")
+                QuatFileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + ("/libQuat.lib" if ZEBuild.Platform.Platform == "Windows" else "/libQuat.a")
+            else:
+                ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib")
+
+                VprnFileSource = self.BuildDirectory + "/libvrpn.a"
+                VprnFileDestination = self.OutputDirectory + "/Lib/libVprn.a"
+                
+                QuatFileSource = self.BuildDirectory + "/quat/libquat.a"
+                QuatFileDestination = self.OutputDirectory + "/Lib/libQuat.a"
+
+            ZEBuild.CopyFile(VprnFileSource, VprnFileDestination)
+            ZEBuild.CopyFile(QuatFileSource, QuatFileDestination)
+
+    def GenerateCMakeList(self):
+        ZEBuild.GenerateCMakeList(self.OutputDirectory, "2.8", "", True)
+
+#Usage : ZEBuild.BuildLibrary(ClassName([LibraryName], [[ExtraLib],[ExtraLib],[ExtraLib]]
+ZEBuild.BuildLibrary(VprnLibrary("Vprn", "SampleExtraLib.lib"))
