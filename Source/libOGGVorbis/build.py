d808f79136ed85af9849b5cf40efd9c2b0544a97
Hakan Candemir <hakan.candemir@zinek.xyz>
2012-12-13 09:50:58 +0000

    FIX: libOGGVorbis/build.py fixed for MacOS build system
    svn path=/; revision=74

--- Source/libOGGVorbis/build.py
+++ Source/libOGGVorbis/build.py
@@ -20,23 +20,24 @@ class OGGVorbisLibrary(ZELibrary):
         ZELibrary.Gather(self, Debug)
         if (Debug != None):
             if not ZEBuild.IsDirectoryExists(self.OutputDirectory + "/Include"):
-                ZEBuild.CopyDirectory(self.BuildDirectory + "/Include", self.OutputDirectory + "/Include")
+                ZEBuild.CopyDirectory(self.SourceDirectory + "/OGG/ogg", self.OutputDirectory + "/Include/ogg")
+                ZEBuild.CopyDirectory(self.SourceDirectory + "/Vorbis/vorbis", self.OutputDirectory + "/Include/vorbis")
                 
             if ZEBuild.Platform.MultiConfiguration:
                 ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release"))
 
-                OggFileSource = self.BuildDirectory + "/OGG" + ("/Debug" if Debug else "/Release") + ("/libOGG.lib" if ZEBuild.Platform.Platform == "Windows" else "/libOGG.a")
+                OggFileSource = self.BuildDirectory + "/OGG" + ("/Debug" if Debug else "/Release") + ("/libOGG.lib" if ZEBuild.Platform.Platform == "Windows" else "/liblibOGG.a")
                 OggFileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + ("/libOGG.lib" if ZEBuild.Platform.Platform == "Windows" else "/libOGG.a")
                 
-                VorbisFileSource = self.BuildDirectory + "/Vorbis" + ("/Debug" if Debug else "/Release") + ("/libVorbis.lib" if ZEBuild.Platform.Platform == "Windows" else "/libVorbis.a")
+                VorbisFileSource = self.BuildDirectory + "/Vorbis" + ("/Debug" if Debug else "/Release") + ("/libVorbis.lib" if ZEBuild.Platform.Platform == "Windows" else "/liblibVorbis.a")
                 VorbisFileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + ("/libVorbis.lib" if ZEBuild.Platform.Platform == "Windows" else "/libVorbis.a")
             else:
                 ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib")
 
-                OggFileSource = self.SourceDirectory + "/lib/libOGG.a"
+                OggFileSource = self.BuildDirectory + "/OGG/liblibOGG.a"
                 OggFileDestination = self.OutputDirectory + "/Lib/libOGG.a"
                 
-                VorbisFileSource = self.SourceDirectory + "/lib/libVorbis.a"
+                VorbisFileSource = self.BuildDirectory + "/Vorbis/liblibVorbis.a"
                 VorbisFileDestination = self.OutputDirectory + "/Lib/libVorbis.a"
 
             ZEBuild.CopyFile(OggFileSource, OggFileDestination)
