c941ee014181b3d605f14ef3f33b346e6d2c0d90
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2012-12-14 11:16:07 +0000

    REM: Old build shell script template _libTemplate has been removed. CHG: libQt has been moved to new ZEPlatform structure. REM: More old build shell scripts has been removed.
    svn path=/; revision=93

--- Source/libQt/Build.sh
+++
@@ -1,8 +0,0 @@
-
-ProjectName=libQt 
-
-echo [ZEBuild Externals] Info : Starting to build external $ProjectName.
-
-Copy Output/$ZE_BUILD_PLATFORM ../../Output/
-
-echo [ZEBuild Externals] Success : $ProjectName build external is completed successfully.
