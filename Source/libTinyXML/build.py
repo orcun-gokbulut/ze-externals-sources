1698a20a6fd4f0b4e6f4afbe33d21c9007753fca
Hakan Candemir <hakan.candemir@zinek.xyz>
2012-12-14 10:58:58 +0000

    FIX: Fixed MacOS output of libTinyXML
    svn path=/; revision=89

--- Source/libTinyXML/build.py
+++ Source/libTinyXML/build.py
@@ -19,12 +19,26 @@ class TinyXMLLibrary(ZELibrary):
                 ZEBuild.CreateDirectory(self.OutputDirectory + "/Include")
                 ZEBuild.CopyFile(self.SourceDirectory + "/tinyxml.h", self.OutputDirectory + "/Include/tinyxml.h")
                 
-            if ZEBuild.Platform.Platform == "Windows" or ZEBuild.Platform.Platform == "MacOS":
+            if ZEBuild.Platform.Platform == "Windows":
                 ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release"))
 
-                FileSource = self.BuildDirectory + ("/Debug" if Debug else "/Release") + ("/libtinyxml.lib" if ZEBuild.Platform.Platform == "Windows" else "/libtinyxml.a")
-                FileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + ("/libTinyXml.lib" if ZEBuild.Platform.Platform == "Windows" else "/libTinyXml.a")
-            else:
+                FileSource = self.BuildDirectory + ("/Debug" if Debug else "/Release") + "/libtinyxml.lib"
+                FileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + "/libTinyXml.lib"
+                
+            elif ZEBuild.Platform.Platform == "MacOS":
+                if ZEBuild.Platform.CMakeGenerator == "Xcode":
+                    ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release"))
+
+                    FileSource = self.BuildDirectory + "/lib" +("/Debug" if Debug else "/Release") + "/libtinyxml.a"
+                    FileDestination = self.OutputDirectory + "/Lib" + ("/Debug" if Debug else "/Release") + "/libTinyXml.a"
+                    
+                elif ZEBuild.Platform.CMakeGenerator == "Unix Makefiles":
+                    ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib")
+
+                    FileSource = self.BuildDirectory + "/lib/libtinyxml.a"
+                    FileDestination = self.OutputDirectory + "/Lib/libTinyXml.a"
+                    
+            elif ZEBuild.Platform.Platform == "Linux":
                 ZEBuild.CreateDirectory(self.OutputDirectory + "/Lib")
 
                 FileSource = self.BuildDirectory + "/lib/libtinyxml.a"
