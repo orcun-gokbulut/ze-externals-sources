6e6d90401d4e9500462336b456af0eec668b1804
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2012-12-14 10:54:46 +0000

    CHG: libCG has been moved to new ZEPlatform structure.
    svn path=/; revision=86

--- Source/libCG/Build.sh
+++
@@ -1,8 +0,0 @@
-
-ProjectName=libCG 
-
-echo [ZEBuild Externals] Info : Starting to build external $ProjectName.
-
-Copy Output/$ZE_BUILD_PLATFORM ../../Output/
-
-echo [ZEBuild Externals] Success : $ProjectName build external is completed successfully.
