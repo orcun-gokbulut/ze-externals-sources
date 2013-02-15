3d08098db6a27c9a6a79c6623b5aad386b5e715e
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2013-02-15 15:45:54 +0000

    FIX: CMake generator bug in Build-Windows-x64-VS2010.bat batch script has been fixed. IMP: Pause statement added to batch scripts.
    svn path=/; revision=121

--- Build-Windows-x64-VS2010.bat
+++ Build-Windows-x64-VS2010.bat
@@ -1,1 +1,2 @@
-python Build.py -p Windows -a x64 -c "Visual Studio 10" -o ./Output
+python Build.py -p Windows -a x64 -c "Visual Studio 10 Win64" -o ./Output
+pause
--- Build-Windows-x86-VS2010.bat
+++ Build-Windows-x86-VS2010.bat
@@ -1,1 +1,2 @@
 python Build.py -p Windows -a x86 -c "Visual Studio 10" -o ./Output
+pause
