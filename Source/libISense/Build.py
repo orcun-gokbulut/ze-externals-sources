ea38454f925b65e6dc99006fc9eb3ed81716246c
Y. Orçun Gökbulut <orcun.gokbulut@zinek.xyz>
2013-05-03 22:11:04 +0000

    ADD: libISense library has been added.
    svn path=/; revision=125

---
+++ Source/libISense/Build.py
@@ -0,0 +1,24 @@
+class libISense(ZELibrary):
+    def Configure(self, Configuration):
+        ZELibrary.Configure(self, Configuration)
+        ZECMake.Configure(self, "", None)
+
+    def Compile(self, Configuration):
+        ZELibrary.Compile(self, Configuration)
+        ZECMake.Build(self, Configuration)
+
+    def Gather(self, Configuration):
+        ZELibrary.Gather(self, Configuration)
+        ZECMake.Install(self, Configuration)
+        ZEOperations.CopyInstallToOutputAuto(self, Configuration)
+        
+        if (Configuration == ZEBuild.CONFIG_DEBUG):
+            ConfigurationString = "/Debug"
+        elif (Configuration == ZEBuild.CONFIG_RELEASE):
+            ConfigurationString = "/Release"
+        else:
+            ConfigurationString = ""
+        ZEOperations.CopyDirectory(self.RootDirectory + "/Prebuild/" + ZEPlatform.PlatformString + "/DLL", self.OutputDirectory + "/DLL" + ConfigurationString)
+
+if (ZEPlatform.Platform == "Windows" or ZEPlatform.Platform == "Linux" or ZEPlatform.Platform == "MacOSX"):
+    ZEBuildDriver.BuildLibrary(libISense("libISense", ""))
